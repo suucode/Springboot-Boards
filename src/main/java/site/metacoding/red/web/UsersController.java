8cae3f1f724575835615512221a5bec6a2e2add3
suucode <limmm.mary@gmail.com>
2022-09-02 17:57:19 +0900

    로그인 시스템 구현 완료

--- src/main/java/site/metacoding/red/web/UsersController.java
+++ src/main/java/site/metacoding/red/web/UsersController.java
@@ -1,10 +1,47 @@
 package site.metacoding.red.web;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import lombok.RequiredArgsConstructor;
+import site.metacoding.red.domain.users.Users;
+import site.metacoding.red.domain.users.UsersDao;
+import site.metacoding.red.web.dto.request.users.JoinDto;
+import site.metacoding.red.web.dto.request.users.LoginDto;
+
+@RequiredArgsConstructor //DI
 @Controller
 public class UsersController {
+	
+	private final HttpSession session;
+	private final UsersDao usersDao; //composition
+	
+	@GetMapping("/logout")
+	public String logout() { //코드로 로그아웃하는것은 세션값을 날린다는것..!
+		session.invalidate();
+		return "redirect:/";
+	}
+	
+	@PostMapping("/login") //로그인만 select지만 post -> select는 원래 get
+	public String login(LoginDto loginDto) {
+		Users usersPS = usersDao.login(loginDto);
+		if(usersPS != null) {//인증이 된 경우 -> 세션이 필요
+			session.setAttribute("principal", usersPS);//principal = 인증된유저
+			return "redirect:/";
+		}else {//인증 안된경우
+			return "redirect:/loginForm";
+		}
+	}
+	
+	@PostMapping("/join")
+	public String join(JoinDto joinDto) {
+		usersDao.insert(joinDto);
+		return "redirect:/loginForm";
+	}
 
 	@GetMapping("/loginForm")
 	public String loginForm() {
--- src/main/webapp/WEB-INF/views/users/loginForm.jsp
+++ src/main/webapp/WEB-INF/views/users/loginForm.jsp
@@ -4,16 +4,17 @@
 <%@ include file="../layout/header.jsp"%>
 
 <div class="container">
-	<form>
+	<form action="/login" method="post">
+	<!-- select중에 유일하게 post를 사용, 쉽게생각하면 그냥 form태그는 다 post -->
 		<div class="mb-3 mt-3">
 			<input
 				type="text" class="form-control"
-				placeholder="Enter username">
+				placeholder="Enter username" name="username">
 		</div>
 		<div class="mb-3">
 			<input
 				type="password" class="form-control" 
-				placeholder="Enter password">
+				placeholder="Enter password" name="password">
 		</div>
 		<button type="submit" class="btn btn-primary">로그인</button>
 	</form>
